481a5ee985aeb1b1ccd9db89bce9e4b6da205179
AVANTHIKA <AVANTHIKA@LAPTOP-KUNHLAJK>
2022-11-13 19:47:53 +0530

    Avanthika

---
+++ src/main/java/ACOECSEA/MavenJenkinsLe4/App.java
@@ -0,0 +1,13 @@
+package ACOECSEA.MavenJenkinsLe4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Jenkins World!" );
+    }
+}
